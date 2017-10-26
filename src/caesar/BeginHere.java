bcedf09f3b44e89524f5a8033405e3e5108369bf
compsci <compsci@compsci>
2017-10-26 10:09:34 -0600

    Added start of JOP option dialog

--- src/caesar/BeginHere.java
+++ src/caesar/BeginHere.java
@@ -2,13 +2,20 @@ package caesar;
 
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 public class BeginHere {
 
 	public static void main(String[] args) {
+		String msg = "Would you like to encode a plain text message?";
+		int confirm = JOptionPane.showConfirmDialog(null, msg);
 		Scanner kbd = new Scanner(System.in);
-
+		
 		CaesarCipher myCipher = new CaesarCipher();
-
+		
+		
+		//if statement
+		
 		System.out.println("Please enter the plain text to encode:");
 		String plainText = kbd.nextLine().toLowerCase();
 
