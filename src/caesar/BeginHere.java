ce355ad91cf28d25076779a2726f075105fe1e32
compsci <compsci@compsci>
2017-10-26 10:00:25 -0600

    Cleaned up code, added grade requirements

--- src/caesar/BeginHere.java
+++ src/caesar/BeginHere.java
@@ -12,9 +12,8 @@ public class BeginHere {
 		System.out.println("Please enter the plain text to encode:");
 		String plainText = kbd.nextLine().toLowerCase();
 
-		System.out.println("Please enter the key: ");
+		System.out.println("Please enter the key:");
 		int key = kbd.nextInt();
-		//System.out.println("Your key is: " + key);
 
 		String codedText = myCipher.encode(plainText, key);
 		System.out.println("Your secret message is: " + codedText);
